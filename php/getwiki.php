0e1e53ca522668bd7aa007e8922c5705a158a745
Jay <jaymollica@gmail.com>
2013-06-03 00:13:55 -0700

    lot of minor changes, now click for images and so on

---
+++ php/getwiki.php
@@ -0,0 +1,60 @@
+<?php
+
+	if($_REQUEST['ant']) {
+		$antReq = $_REQUEST['ant'];
+	}
+	else {
+		print '<p>No images available.</p>';
+		exit;
+	}
+
+	$family = 'ant';
+	$subfamilia = 'Leptanillinae';
+
+
+	$imagesUrl = 'http://en.wikipedia.org/w/api.php?action=query&format=json&titles=' . $antReq . '&prop=images';
+
+	$content = file_get_contents($imagesUrl); 
+	$array = json_decode($content, TRUE);
+
+	$page_images = array();
+
+	$i = 0;
+	foreach($array['query']['pages'] AS $page) {
+		foreach($page['images'] AS $img) {
+			$img['title'] = preg_replace('/File:/i','',$img['title']);
+			$page_images[$i] = urlencode($img['title']);
+			$i++;
+		}
+	}
+
+	$n = 0;
+	foreach($page_images AS $pi) {
+
+		$imgUrl = 'http://en.wikipedia.org/w/api.php?action=query&format=json&titles=Image:' . $pi . '&prop=imageinfo&iiprop=url';
+
+		$imgContent = file_get_contents($imgUrl);
+
+		$array = json_decode($imgContent, TRUE);
+
+		
+
+		foreach($array['query']['pages'] AS $page) {
+			foreach($page['imageinfo'] AS $info) {
+				if(preg_match('/.webm/i',$info['url'])) {
+					print '<a href="' . $info['url'] . '"><video width="200px" ><source src="' . $info['url'] . '" type="video/webm" /></video></a>';
+					$n++;
+				}
+				elseif(!preg_match('/.svg/i',$info['url'])) {
+					print '<a href="' . $info['descriptionurl'] . '"><img src="' . $info['url'] . '" width="400px" /></a>';
+					$n++;
+				}
+			}
+		}
+	}
+
+	if($n == 0) {
+		print '<p>No images available.</p>';
+	}
+
+?>
