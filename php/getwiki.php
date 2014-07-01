cff8a4e10749ea244bd399a6b84493eede163b2b
jmollica <jmollica@calacademy.org>
2014-07-01 11:09:37 -0700

    updates to limit returns and clean up data for v2

--- php/getwiki.php
+++ php/getwiki.php
@@ -1,13 +1,13 @@
 <?php
 
-	if($_REQUEST['ant'] && $_REQUEST['depth']) {
+	if(isset($_REQUEST['ant']) && isset($_REQUEST['depth'])) {
 		$antReq = $_REQUEST['ant'];
 		$depth = $_REQUEST['depth'];
 	}
 	else {
 		exit;
 	}
-	
+
 	$urlRoot = 'http://www.antweb.org/api/v2';
 
 	//we infer rank from depth of name
@@ -23,21 +23,24 @@
 
 	$imagesUrl = $urlRoot . '/?' . $rank . '=' . $antReq;
 
-
-
-	$content = file_get_contents($imagesUrl); 
+	$content = file_get_contents($imagesUrl);
 	$ants = json_decode($content, TRUE);
+	shuffle($ants['specimens']);
+	$count = count($ants['specimens']);
 
+	print '<div class="specimen_box>';
+	print '<p>The query for the ' . $rank . ' <em>' . $antReq . '</em> returned ' . $count . 'records.</p>';
+	print '</div>';
 
+	$i = 0;
 	foreach($ants['specimens'] AS $ant) {
-		if($ant['images']) {
-			$i = 1;
+		if(isset($ant['images'])) {
 			$imgs = $ant['images'];
 
 			print '<div class="specimen_box">';
-			print '<h3>' . ucfirst($ant['meta']['subfamily']) . ' ' . ucfirst($ant['meta']['genus']) . ' ' . $ant['meta']['species'] . '</h3>';
-			print '<p class="taxonomy">' . $ant['meta']['code'] . '</p>';
-			
+			print '<h3>' . ucfirst($ant['subfamily']) . ' ' . ucfirst($ant['genus']) . ' ' . $ant['specificEpithet'] . '</h3>';
+			print '<p class="taxonomy"><a href="' . $ant['url'] . '" target="_blank">' . $ant['catalogNumber'] . '</a></p>';
+
 			foreach($imgs AS $img) {
 				foreach($img['shot_types'] AS $type) {
 
@@ -61,11 +64,13 @@
 			}
 
 			print '</div>';
+			$i++;
+			if($i == 20) { break; }
 		}
-		
+
 	}
 
-	if(!$i) {
+	if(!isset($i)) {
 		print '<p>url: ' . $imagesUrl . '</p>';
 		print '<p>No images available for <i>' . $antReq . '</i>.</p>';
 	}
